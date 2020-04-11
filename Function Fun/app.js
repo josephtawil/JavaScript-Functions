fc1d3c5442cb596f24ffe411f66833c67af67973
Joseph Tawil <jtawil2015@gmail.com>
2020-04-11 11:27:24 -0700

    created a sayHello Function

---
+++ Function Fun/app.js
@@ -0,0 +1,12 @@
+// we are creating a function called sayHello that console logs Hello World
+// sayHello is the name of the function
+//function says that sayHello is a function
+function sayHello()
+{
+    console.log("Hello World");
+}
+//say hello world 10 times
+for(var i = 0; i <= 10; i++)
+{
+    sayHello();
+}
