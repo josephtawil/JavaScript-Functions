a84b874a003d5e8fbd22dd111fd01c3738ae498f
Joseph Tawil <jtawil2015@gmail.com>
2020-04-11 13:01:57 -0700

    making objects and functions

---
+++ Objects/objects.js
@@ -0,0 +1,37 @@
+//create object and assigning values
+var click = 
+var car =
+{
+    doors: 2,
+    mileage: 0,
+    mph: 0,
+    showMiles: function()
+        {
+            console.log(this.mileage);
+
+        },
+    honkHorn: function()
+        {
+            console.log("HONKKKKKK!!!!");
+        },
+    accelerate: function()
+        {
+            if()
+        },
+
+};
+
+//you are accessing door value by using car.doors
+car.doors;
+//prints out the number of doors
+console.log(car.doors);
+//accessing mileage of car
+// car.mileage;
+// //printing out value of mileage
+// console.log(car.mileage);
+
+car.showMiles();
+
+//create a method that honks a horn
+
+car.honkHorn();
